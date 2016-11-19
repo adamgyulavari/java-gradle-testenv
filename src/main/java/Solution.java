feab9dcccf4ad332401366cd53cdd4da462166ca
Adam Gyulavari <adam.gyulavari@lab.coop>
2016-11-19 13:48:20 +0100

    init project

---
+++ src/main/java/Solution.java
@@ -0,0 +1,9 @@
+/**
+ * Created by aze on 2016.11.19..
+ */
+public class Solution implements TestWorkshop {
+
+    public int testable(int i) {
+        return i + 2;
+    }
+}
---
+++ src/main/java/TestWorkshop.java
@@ -0,0 +1,6 @@
+/**
+ * Created by aze on 2016.11.19..
+ */
+public interface TestWorkshop {
+    int testable(int i);
+}
